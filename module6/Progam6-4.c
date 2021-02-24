18ad8cf8941ce76403887faa8d66512ca2325144
ckbsbal <58983655+ckbsbal@users.noreply.github.com>
2021-02-24 22:39:55 +0530

    Update Progam6-4.c

--- module6/Progam6-4.c
+++ module6/Progam6-4.c
@@ -2,7 +2,7 @@
 int main()
 {
 int n,fact=1,i,n1=2,flag=1;
-double sum=0.0;
+double sum=1.0;
 printf("enter a number=");
 scanf("%d",&n);
 
@@ -11,7 +11,7 @@ while(n+1!=n1)
 {
 	 for(i=1;i<=n1+1;i++)   
       fact=fact*i;
- if(flag%2==1)
+ if(flag%2==0)
 	  sum=sum+(double)n1/fact;
 	  else
 	  sum=sum-(double)n1/fact;  
@@ -20,7 +20,6 @@ while(n+1!=n1)
 	fact=1;
 
 }
-sum=sum-1;
 printf("\nsum=%lf",sum);
 return 0;
 }
