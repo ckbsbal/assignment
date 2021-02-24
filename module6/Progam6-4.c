6b2db75df80d72a04706f4a9e4013e27937c4496
ckbsbal <58983655+ckbsbal@users.noreply.github.com>
2021-02-24 22:37:29 +0530

    Create Progam6-4.c

---
+++ module6/Progam6-4.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int n,fact=1,i,n1=2,flag=1;
+double sum=0.0;
+printf("enter a number=");
+scanf("%d",&n);
+
+
+while(n+1!=n1)
+{
+	 for(i=1;i<=n1+1;i++)   
+      fact=fact*i;
+ if(flag%2==1)
+	  sum=sum+(double)n1/fact;
+	  else
+	  sum=sum-(double)n1/fact;  
+    n1++;
+	 flag++;
+	fact=1;
+
+}
+sum=sum-1;
+printf("\nsum=%lf",sum);
+return 0;
+}
